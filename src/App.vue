2dc98b275c92803fc435de680d58c6086c02fcdc
HugoGross <hugobombeirobbs@gmail.com>
2023-12-10 22:06:15 -0300

    asd

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,46 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+const url = ("https://www.youtube.com/watch?v=BbeeuzU5Qc8")
 
-const contador = ref(0)
+    const nome = 'Hugo'
+    const idade = 18
 
-function incrementarContador() {
-  contador.value++
-}
-function decrementarContador() {
-  contador.value--
-}
-function zerarContador() {
-  contador.value = 0
-}
+    function inverter(texto) {
+        return texto.split('').reverse().join('')
+    }
 
-onMounted(() => {
-  console.log(`O valor inicial do contador é ${contador.value}.`)
-})
+    function saudacao() {
+        return `Olá, ${nome}!`
+    }
 </script>
 
 <template>
-<button @click="incrementarContador">Incrementar</button>
-<button @click="decrementarContador">Decrementar</button>
-<button @click="zerarContador">Zerar</button>
-<p>Valor do contador é {{ contador }}</p>
+    <div class="info">
+        <h1>Exemplo 1 - Mostrando informações na tela</h1>
+        <p>Nome: {{ nome }} </p>
+        <p>Idade: {{ idade }}  anos</p>
+        <hr />
+        <p>Para completar 50 anos faltam: {{ 50 - idade }}  anos</p>
+        <p>O nome tem {{ nome.length }}  caracteres</p>
+        <p>O nome invertido é: {{ inverter(nome) }} </p>
+        <hr />
+        <p> Exemplo de saudação usando função</p>
+        <p> {{ saudacao() }}  </p>
+        <a :href="url">Mais informações</a>
+    </div>
+    
 </template>
 
 <style scoped>
-button {
+.info {
+  background-color: rgba(35,12,22,1);
+  padding: 20px 30px;
+  color: rgb(183, 210, 219);
+  border-radius: 10px;
+}
+
+h1 {
+  color: white;
   font-weight: bold;
+  font-size: 1.5em;
 }
 </style>
